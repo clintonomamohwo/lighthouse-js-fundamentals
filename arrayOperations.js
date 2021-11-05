a8e42f6ed8839d6b4a3c373c7d0fcca6a9f0ea4b
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2021-11-05 11:20:48 -0400

    Practise

---
+++ arrayOperations.js
@@ -0,0 +1,24 @@
+
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+
+"My favourite " + director + " film is " + films[1];
+"films at index 0: " + films[0];
+"films at index 1: " + films[1];
+"films at index 2: " + films[2];
+"films at index 3: " + films[3];
+
+const oddNumbers = [1, 3, 5, 7, 9];
+const randomStuff = [1.15, "bicycle", true];
+
+films[2] = "Jurassic Park";
+films;
+films.push("A.I.");
+films;
+
+console.log(director + " is the director of " + films.length + " films");
+
+films.push("E.T.");
+console.log(director + " is the director of " + films.length + " films");
+console.log(5==="5");
