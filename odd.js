d8fd91ee7e6292807d72aee32d44f3166157d982
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2021-11-05 11:27:35 -0400

    All

--- odd.js
+++ odd.js
@@ -1,4 +1,4 @@
-const isOdd=function(num)
+/*const isOdd=function(num)
 {
   if (num%2==1)
   {
@@ -9,3 +9,15 @@ const isOdd=function(num)
 }
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
+*/
+
+let sum = 3 + 2;
+
+const doubleSum = function(a, b) {
+  let sum = a + b;
+  return sum * 2;
+}
+
+doubleSum(5, 4);
+
+console.log(sum);
